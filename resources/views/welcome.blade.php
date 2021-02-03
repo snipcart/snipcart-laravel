5fac13e6afa8eab429dc25fc4e438b2915b13935
pierreguillaumelaurin <pierre-guillaumelaurin@Pierre-Guillaumes-MacBook-Pro.local>
2021-02-03 13:46:40 -0500

    Add preconnect hints

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,23 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
+
+<head>
     <!-- TODO upgrade to v3.0.28 -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Snipcart &amp; Laravel demo</title>
+    <title>Snipcart &amp; Laravel demo</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet" />
-        <link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet" />
-        <link rel="shortcut icon" href="/img/Logo.svg"/>
-        <link rel="stylesheet" href="https://cdn.snipcart.com/themes/v3.0.28/default/snipcart.css" />
+    <link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet" />
+    <link rel="shortcut icon" href="/img/Logo.svg" />
 
-        <link rel="stylesheet" type="text/css" href="/css/app.css" />
-    </head>
-    <body>
-        <div id="app"></div>
-        <div hidden id="snipcart" data-api-key="MzMxN2Y0ODMtOWNhMy00YzUzLWFiNTYtZjMwZTRkZDcxYzM4"></div>
-        <script async src="https://cdn.snipcart.com/themes/v3.0.28/default/snipcart.js"></script>
-        <script type="text/javascript" src="/js/app.js"></script>
-    </body>
+    <link rel="preconnect" href="https://app.snipcart.com">
+    <link rel="preconnect" href="https://cdn.snipcart.com">
+
+    <link rel="stylesheet" href="https://cdn.snipcart.com/themes/v3.0.28/default/snipcart.css" />
+
+    <link rel="stylesheet" type="text/css" href="/css/app.css" />
+</head>
+
+<body>
+    <div id="app"></div>
+    <div hidden id="snipcart" data-api-key="MzMxN2Y0ODMtOWNhMy00YzUzLWFiNTYtZjMwZTRkZDcxYzM4"></div>
+    <script async src="https://cdn.snipcart.com/themes/v3.0.28/default/snipcart.js"></script>
+    <script type="text/javascript" src="/js/app.js"></script>
+</body>
+
 </html>
